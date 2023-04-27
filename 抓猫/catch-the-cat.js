10870ebf7622ce84615fbdec1d460d2c13c77cae
YJX <20300220007@fudan.edu.cn>
2023-04-27 16:56:31 +0800

    Site updated: 2023-04-27 16:56:29

--- 抓猫/catch-the-cat.js
+++ 抓猫/catch-the-cat.js
@@ -1,4 +1,4 @@
-!(function (t) {
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>!(function (t) {
   var e = {};
 
   function n(o) {
